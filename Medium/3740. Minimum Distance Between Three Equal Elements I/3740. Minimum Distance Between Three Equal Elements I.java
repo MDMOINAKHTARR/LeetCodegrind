1aef54aca66c1b65988d695b168d059cb70b7721
MD MOIN AKHTAR <164334281+MDMOINAKHTARR@users.noreply.github.com>
2026-04-13 22:09:52 +0530

    Create 3740. Minimum Distance Between Three Equal Elements I.java

---
+++ Medium/3740. Minimum Distance Between Three Equal Elements I/3740. Minimum Distance Between Three Equal Elements I.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        Map<Integer, java.util.List<Integer>> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        
+        int ans = Integer.MAX_VALUE;
+        
+        for (java.util.List<Integer> list : map.values()) {
+            if (list.size() >= 3) {
+                for (int i = 0; i <= list.size() - 3; i++) {
+                    int dist = list.get(i + 2) - list.get(i);
+                    ans = Math.min(ans, dist);
+                }
+            }
+        }
+        
+        return ans == Integer.MAX_VALUE ? -1 : 2 * ans;
+    }
+}
