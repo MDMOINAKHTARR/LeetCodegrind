a4982c63dcca4eeab099fa7f02ef4a691bf7bbc7
MD MOIN AKHTAR <164334281+MDMOINAKHTARR@users.noreply.github.com>
2026-06-28 01:16:52 +0530

    Create 3020. Find the Maximum Number of Elements in Subset.java

---
+++ Medium/3020. Find the Maximum Number of Elements in Subset/3020. Find the Maximum Number of Elements in Subset.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int maximumLength(int[] nums) {
+        Map<Long, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put((long) num, freq.getOrDefault((long) num, 0) + 1);
+        }
+
+        int ans = 1;
+
+        // Special case for 1
+        if (freq.containsKey(1L)) {
+            int cnt = freq.get(1L);
+            ans = Math.max(ans, (cnt % 2 == 0) ? cnt - 1 : cnt);
+        }
+
+        for (long start : freq.keySet()) {
+            if (start == 1) continue;
+
+            long cur = start;
+            int len = 0;
+
+            while (freq.getOrDefault(cur, 0) >= 2) {
+                len += 2;
+                cur = cur * cur;
+            }
+
+            if (freq.getOrDefault(cur, 0) >= 1) {
+                len += 1;
+            } else if (len > 0) {
+                len -= 1;
+            }
+
+            ans = Math.max(ans, len);
+        }
+
+        return ans;
+    }
+}
